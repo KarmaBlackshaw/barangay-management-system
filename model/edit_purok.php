21f96184a46362c31e9051e516acd83d4bcc3811
Karma Blackshaw <irenesejah29@gmail.com>
2022-05-18 18:32:20 +0800

    lint edit purok

--- model/edit_purok.php
+++ model/edit_purok.php
@@ -1,32 +1,30 @@
 <?php
-include('../server/server.php');
+include "../server/server.php";
 
-if (!isset($_SESSION['username'])) {
-    if (isset($_SERVER["HTTP_REFERER"])) {
-        header("Location: " . $_SERVER["HTTP_REFERER"]);
-    }
+if (!isset($_SESSION["username"])) {
+	if (isset($_SERVER["HTTP_REFERER"])) {
+		header("Location: " . $_SERVER["HTTP_REFERER"]);
+	}
 }
 
-$purok         = $conn->real_escape_string($_POST['purok']);
-$details     = $conn->real_escape_string($_POST['details']);
-$id         = $conn->real_escape_string($_POST['id']);
+$purok = $conn->real_escape_string($_POST["purok"]);
+$details = $conn->real_escape_string($_POST["details"]);
+$id = $conn->real_escape_string($_POST["id"]);
 
 if (!empty($purok)) {
+	$query = "UPDATE purok SET `name` = '$purok', `details`='$details' WHERE id=$id;";
+	$result = $conn->query($query);
 
-    $query         = "UPDATE tblpurok SET `name` = '$purok', `details`='$details' WHERE id=$id;";
-    $result     = $conn->query($query);
-
-    if ($result === true) {
-        $_SESSION['message'] = 'Purok has been updated!';
-        $_SESSION['status'] = 'success';
-    } else {
-        $_SESSION['message'] = 'Something went wrong!';
-        $_SESSION['status'] = 'danger';
-    }
+	if ($result === true) {
+		$_SESSION["message"] = "Purok has been updated!";
+		$_SESSION["status"] = "success";
+	} else {
+		$_SESSION["message"] = "Something went wrong!";
+		$_SESSION["status"] = "danger";
+	}
 } else {
-
-    $_SESSION['message'] = 'No purok ID found!';
-    $_SESSION['status'] = 'danger';
+	$_SESSION["message"] = "No purok ID found!";
+	$_SESSION["status"] = "danger";
 }
 
 header("Location: ../purok.php");
