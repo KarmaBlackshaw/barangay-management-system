96cfaba181b08d0f3ef6b08613ae71a73dff0141
Karma Blackshaw <irenesejah29@gmail.com>
2022-05-18 18:29:57 +0800

    add view  helpers

--- bootstrap/index.php
+++ bootstrap/index.php
@@ -8,3 +8,4 @@ include PATH_ROOT . "/server/server.php";
 include PATH_ROOT . "/helpers/role.php";
 include PATH_ROOT . "/helpers/method-vars.php";
 include PATH_ROOT . "/helpers/array-helpers.php";
+include PATH_ROOT . "/helpers/view-helpers.php";
