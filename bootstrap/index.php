de98bac061a0b66505158ae31f2deff8951482de
Karma Blackshaw <irenesejah29@gmail.com>
2022-05-18 18:07:33 +0800

    bootstrap array helpers

--- bootstrap/index.php
+++ bootstrap/index.php
@@ -1,9 +1,10 @@
 <?php
 
-define("PATH_ROOT", realpath(__DIR__ . '/..'));
+define("PATH_ROOT", realpath(__DIR__ . "/.."));
 
-require PATH_ROOT . '/vendor/autoload.php';
+require PATH_ROOT . "/vendor/autoload.php";
 
 include PATH_ROOT . "/server/server.php";
 include PATH_ROOT . "/helpers/role.php";
 include PATH_ROOT . "/helpers/method-vars.php";
+include PATH_ROOT . "/helpers/array-helpers.php";
