55ae4919b74b409828b5931e2b9324f11d8f5b70
Karma Blackshaw <irenesejah29@gmail.com>
2022-05-19 16:00:26 +0800

    add new helper

--- helpers/view-helpers.php
+++ helpers/view-helpers.php
@@ -1,10 +1,28 @@
 <?php
 
-function imgSrc($src)
+function imgSrc($src, $default = "")
 {
 	if (empty($src)) {
+		if (!empty($default)) {
+			return "assets/" . $default;
+		}
+
 		return null;
 	}
 
 	return preg_match("/data:image/i", $src) ? $src : "assets/uploads/" . $src;
+}
+
+function ifThen($condition, $then)
+{
+	return $condition ? $then : null;
+}
+
+function fullname($row)
+{
+	$fname = $row["firstname"];
+	$mname = $row["middlename"];
+	$lname = $row["lastname"];
+
+	return ucwords("$fname $mname $lname");
 }
