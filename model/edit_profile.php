2ddab406c357a2d26a2fbe07203fb27d2d3200d2
Karma Blackshaw <irenesejah29@gmail.com>
2022-05-25 17:07:32 +0800

    fix: edit profile bug

--- model/edit_profile.php
+++ model/edit_profile.php
@@ -1,5 +1,5 @@
 <?php
-include "../server/server.php";
+include "../bootstrap/index.php";
 
 if (!isset($_SESSION["username"])) {
 	if (isset($_SERVER["HTTP_REFERER"])) {
@@ -7,51 +7,41 @@ if (!isset($_SESSION["username"])) {
 	}
 }
 
-$id = $conn->real_escape_string($_POST["id"]);
-$profile = $conn->real_escape_string($_POST["profileimg"]); // base 64 image
-$profile2 = $_FILES["img"]["name"];
-// change profile2 name
-$newName = date("dmYHis") . str_replace(" ", "", $profile2);
+/**
+ * Handle profile image
+ */
+$id = getBody("id", $_POST); // base 64 image
+$profileCamera = getBody("profileimg", $_POST); // base 64 image
+$profileFile = $_FILES["img"];
 
-// image file directory
-$target = "../assets/uploads/avatar/" . basename($newName);
+$imgFilename = empty($profileCamera) ? null : $profileCamera;
 
-if (!empty($id)) {
-	$query = "SELECT * FROM users WHERE id='$id'";
-	$res = $conn->query($query);
+if ($profileFile["name"]) {
+	$uniqId = uniqid(date("YmdhisU"));
+	$ext = pathinfo($profileFile["name"], PATHINFO_EXTENSION);
+	$imgFilename = "$uniqId.$ext";
+	$imgDir = "../assets/uploads/$imgFilename";
 
-	if ($res->num_rows == 0) {
-		$_SESSION["message"] = "User not found!";
-		$_SESSION["status"] = "danger";
-
-		if (isset($_SERVER["HTTP_REFERER"])) {
-			header("Location: " . $_SERVER["HTTP_REFERER"]);
-		}
-	} else {
-		if (!empty($profile) && !empty($profile2)) {
-			$insert = "UPDATE users SET avatar='$profile' WHERE id='$id'";
-			$result = $conn->query($insert);
-			$_SESSION["avatar"] = $profile;
-		} elseif (!empty($profile) && empty($profile2)) {
-			$insert = "UPDATE users SET avatar='$profile' WHERE id='$id'";
-			$result = $conn->query($insert);
-			$_SESSION["avatar"] = $profile;
-		} else {
-			$insert = "UPDATE users SET avatar='$newName' WHERE id='$id'";
-			$result = $conn->query($insert);
-			move_uploaded_file($_FILES["img"]["tmp_name"], $target);
-			$_SESSION["avatar"] = $newName;
-		}
-
-		$_SESSION["message"] = "Profile has been updated! Please login again!";
-		$_SESSION["status"] = "success";
-	}
-} else {
-	$_SESSION["message"] = "Please fill up the form completely!";
-	$_SESSION["status"] = "danger";
+	move_uploaded_file($profileFile["tmp_name"], $imgDir);
 }
+
+echo "<pre>";
+var_dump($imgFilename);
+echo "</pre>";
+
+$result = $db
+	->update("users")
+	->where("id", $id)
+	->set([
+		"avatar" => $imgFilename,
+	])
+	->exec();
+$_SESSION["avatar"] = $imgFilename;
+$_SESSION["message"] = "Profile has been updated! Please login again!";
+$_SESSION["status"] = "success";
 
 if (isset($_SERVER["HTTP_REFERER"])) {
 	header("Location: " . $_SERVER["HTTP_REFERER"]);
 }
+
 $conn->close();
