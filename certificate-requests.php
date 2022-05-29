03b2084e70e2c831361e78ab6064238acdec6c20
karmablackshaw <irenesejah29@gmail.com>
2022-05-29 19:25:08 +0800

    update: staff can generate certificate

--- certificate-requests.php
+++ certificate-requests.php
@@ -159,45 +159,29 @@ $request_list = (function () use ($db) {
                             </td>
                             <td class="d-flex justify-content-center align-items-center gap-3">
                               <?php if (isUser()): ?>
-                              <a
-                                href="javascript:void(0)"
-                                data-target="#edit-request"
-                                data-value-id="<?= $request["id"] ?>"
-                                data-value-memo="<?= $request["memo"] ?>"
-                                data-value-certificate_id="<?= $request["certificate_id"] ?>"
-                                onclick="showModal(this)"
-                              >
+                              <a href="javascript:void(0)" data-target="#edit-request"
+                                data-value-id="<?= $request["id"] ?>" data-value-memo="<?= $request["memo"] ?>"
+                                data-value-certificate_id="<?= $request["certificate_id"] ?>" onclick="showModal(this)">
                                 <i class="fa fa-edit"></i>
                               </a>
                               <?php endif; ?>
 
-                              <?php if (isAdmin()): ?>
-                              <a
-                                href="javascript:void(0)"
-                                data-target="#edit-request"
-                                data-value-id="<?= $request["id"] ?>"
-                                data-value-memo="<?= $request["memo"] ?>"
-                                data-value-certificate_id="<?= $request["certificate_id"] ?>"
-                                onclick="showModal(this)"
-                              >
+                              <?php if (role(['administrator', 'staff'])): ?>
+                              <a href="javascript:void(0)" data-target="#edit-request"
+                                data-value-id="<?= $request["id"] ?>" data-value-memo="<?= $request["memo"] ?>"
+                                data-value-certificate_id="<?= $request["certificate_id"] ?>" onclick="showModal(this)">
                                 <i class="fa fa-eye"></i>
                               </a>
                               <?php endif; ?>
 
-                              <?php if (isAdmin() && $request["status"] !== "resolved"): ?>
-                              <a
-                                href="<?= $request["certificate_url"] ?>"
-                                class="btn-link btn-info"
-                              >
+                              <?php if (role(['administrator', 'staff']) && $request["status"] !== "resolved"): ?>
+                              <a href="<?= $request["certificate_url"] ?>" class="btn-link btn-info">
                                 <i class="fa fa-file"></i>
                               </a>
                               <?php endif; ?>
 
                               <?php if (role(["user", "administrator"])): ?>
-                              <a
-                                data-toggle="tooltip"
-                                data-original-title="Remove"
-                                href="model/certificate-request.php?id=<?= $request[
+                              <a data-toggle="tooltip" data-original-title="Remove" href="model/certificate-request.php?id=<?= $request[
                                 	"id"
                                 ] ?>&delete-request=1"
                                 onclick="confirm('Are you sure you want to delete this blotter?');"
